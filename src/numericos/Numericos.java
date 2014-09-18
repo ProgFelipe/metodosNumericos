93cc7b8c31d6dfc2935e2d580c7fc15aa67b01c5
felipegi91 <felipegi91@gmail.com>
2014-09-18 23:54:06 +0200

    frame y elementos iniciales para la interacción con el usuario.

--- src/numericos/Numericos.java
+++ src/numericos/Numericos.java
@@ -5,17 +5,87 @@
  */
 package numericos;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JScrollPane;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
+import metodos.*;
+
 /**
  *
- * @author Felipe
+ * @authors Felipe & Andres
  */
 public class Numericos {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    private static JTextField jtxtfuncion;
+    private static JButton btncalcular;
+    private static JList list;
+    public funcion funcion;
+    public metodos.puntoFijo puntoFijo;
+    public metodos.bsqIncremental bsqIncremental;
+    public metodos.biseccion biseccion;
+    public metodos.NewtonRaphson NewtonRaphson;
+    public metodos.secante secante;
     
+    
+    public static void main(String[] args) {
+      // Crear un nuevo Frame
+      final JFrame frame = new JFrame("Métodos Numéricos - Andres & Felipe 0.1");
+      String [] data = {"Busqueda incremental","Punto fijo","Biseccion","Newton Raphson","Secante"};
+      jtxtfuncion = new JTextField("ingrese funcion",20);
+      btncalcular = new JButton("Evaluar");
+      list = new JList(data); //data has type Object[]
+      list.setSelectionMode(ListSelectionModel.SINGLE_INTERVAL_SELECTION);
+      list.setLayoutOrientation(JList.VERTICAL);
+      list.setVisibleRowCount(-1);
+      
+      JScrollPane listScroller = new JScrollPane(list);
+      
+      listScroller.setPreferredSize(new Dimension(250, 80));
+      
+      btncalcular.addActionListener(new ActionListener(){
+          public void actionPerformed(ActionEvent e) {
+              funcion f = new funcion(jtxtfuncion.getText());
+              if(f.validador()){
+                    switch(list.getSelectedIndex()){
+                        default:
+                            new bsqIncremental();
+                            break;
+                        case 1:
+                            new puntoFijo();
+                            break;
+                        case 2:
+                            new biseccion();
+                            break;                      
+                        case 3:
+                            new NewtonRaphson();
+                            break;                      
+                        case 4:
+                            new secante();
+                            break;
+                    }
+              }
+          }
+      });
+      // Al cerrar el frame, termina la ejecuciÃ³n de este programa
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      Dimension screenSize = java.awt.Toolkit.getDefaultToolkit().getScreenSize();  
+      // Agregar un JPanel que se llama Mesh (esta clase)
+      frame.setLayout( new FlowLayout( ) );
+      frame.add(jtxtfuncion);
+      frame.add(listScroller);
+      frame.add(btncalcular);
+      // Asignarle tamaÃ±o
+      frame.setSize(screenSize.width/2, 140);
+      // Poner el frame en el centro de la pantalla
+      frame.setLocationRelativeTo(null);
+      // Mostrar el frame
+      frame.setVisible(true);
+    }   
 }
