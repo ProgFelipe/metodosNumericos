f9a390be27b8a16f1f26e3461600876ecb59db28
felipegi91 <felipegi91@gmail.com>
2014-09-18 23:54:53 +0200

    clase funcion para evaluar la entrada y hacer los calculos de evaluacion de la funcion.

---
+++ src/numericos/funcion.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numericos;
+
+/**
+ *
+ * @author Felipe & Andres
+ */
+public class funcion {
+    
+    public funcion(String f){
+    
+    }
+    
+    public boolean validador(){
+        return false;
+    }
+    
+    public double calc(double x){
+        
+        return 0;
+    }
+}
