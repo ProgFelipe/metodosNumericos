8055e52c630f8d2ffd3f2be9e42871a15410200c
felipegi91 <felipegi91@gmail.com>
2014-09-25 15:50:16 -0500

    biseccion arreglando los mensajes.

--- src/numericos/funcion.java
+++ src/numericos/funcion.java
@@ -21,7 +21,8 @@ public class funcion {
         //System.err.println(x+" "+Math.sin(x));
         //return Math.sin(x);
         //e^(3x-12)+xcos(3x)-x^2+4 = 0
-        return Math.exp(3*x-12)+x*Math.cos(3*x)-Math.pow(x, 2)+4;
+        //return Math.exp(3*x-12)+x*Math.cos(3*x)-Math.pow(x, 2)+4;
+        return Math.pow(x, 3) + (4*Math.pow(x, 2)) - 10;
     }
     //Derivada f'(X)
     public double derivada(double x){
